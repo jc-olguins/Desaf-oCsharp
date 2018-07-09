dfe6284a80ce73e573ddbcd681483d244608cf02
Julio Olguin <Julio.Olguin@ipsos.com>
2018-07-09 09:20:29 -0500

    Lectura  de archivo de text y escritura en un archivo Binario

--- Desafio1/Program.cs
+++ Desafio1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Desafio1.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,33 @@ namespace Desafio1
     {
         static void Main(string[] args)
         {
+            var listaProfes = new List<Profesor>();
+            string[] lineas = File.ReadAllLines("./Files/Profesores.txt");
+
+            int localId = 0;
+
+            foreach (var linea in lineas)
+            {
+                listaProfes.Add(new Profesor { Nombre = linea, Id = localId++ });
+            }
+
+            foreach (var profe in listaProfes)
+            {
+                Console.WriteLine(profe.Nombre);
+            }
+
+            var archivo = File.Open("profesBinarios.bin", FileMode.OpenOrCreate);
+
+            var binFile = new BinaryWriter(archivo);
+
+            foreach (var profe in listaProfes)
+            {
+                binFile.Write(profe.Nombre);
+                binFile.Write(profe.Id);
+            }
+
+            binFile.Close();
+            archivo.Close();
         }
     }
 }
